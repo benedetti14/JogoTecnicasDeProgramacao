42a50b63b5393d625ff7ff27eb4c44908716de26
benedetti14 <gabrieleliasbenedetti@hotmail.com>
2022-11-02 20:32:32 -0300

    Atualiza o gerenciador grafico

--- JogoTeste/Gerenciador_Grafico.cpp
+++ JogoTeste/Gerenciador_Grafico.cpp
@@ -1,1 +1,51 @@
 #include "Gerenciador_Grafico.h"
+#include <stdlib.h>
+#include <iostream>
+
+GerenciadorGrafico* GerenciadorGrafico::pGrafico = NULL;
+
+GerenciadorGrafico::GerenciadorGrafico() {
+
+	janela = new sf::RenderWindow(sf::VideoMode(700, 700), "janela");
+	if (janela == NULL) {
+		std::cout << "Erro na criação da janela!";
+		return;
+	}
+}
+
+GerenciadorGrafico::~GerenciadorGrafico() {
+	if (janela) {
+		delete (janela);
+		janela = NULL;
+	}
+}
+
+GerenciadorGrafico* GerenciadorGrafico::getGerenciadorGrafico() {
+	if (pGrafico == NULL) {
+		return new GerenciadorGrafico();
+	}
+	return pGrafico;
+}
+
+sf::RenderWindow* GerenciadorGrafico::getJanela() {
+	return pGrafico->janela;
+}
+
+const bool GerenciadorGrafico::janelaAberta() {
+	if (janela->isOpen()) {
+		return true;
+	}
+	return false;
+}
+
+void GerenciadorGrafico::fechaJanela() {
+	janela->close();
+}
+
+void GerenciadorGrafico::limpaJanela() {
+	janela->clear();
+}
+
+void GerenciadorGrafico::mostrar() {
+	janela->display();
+}
--- JogoTeste/Gerenciador_Grafico.h
+++ JogoTeste/Gerenciador_Grafico.h
@@ -1,5 +1,18 @@
 #pragma once
-class Gerenciador_Grafico
-{
-};
+#include <SFML/Graphics.hpp>
 
+
+class GerenciadorGrafico {
+private:
+	sf::RenderWindow* janela;
+	static GerenciadorGrafico* pGrafico;
+	GerenciadorGrafico();
+public:
+	~GerenciadorGrafico();
+	static GerenciadorGrafico* getGerenciadorGrafico();
+	sf::RenderWindow* getJanela();
+	const bool janelaAberta();
+	void fechaJanela();
+	void limpaJanela();
+	void mostrar();
+};
